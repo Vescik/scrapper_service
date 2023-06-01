3b707a924d7c4ed625a009a3af60f3150aec29f2
Vescik <wescik992@gmail.com>
2023-06-01 18:45:26 +0200

    fix controller bugs

--- controllers/scrappingController.ts
+++ controllers/scrappingController.ts
@@ -1,13 +1,13 @@
 const scraperService = require('../services/scraperService');
 
 const scrapingController = {
-    ascync saveScrapData(req,res){
+    async saveScrapData(req,res){
         try{
             const scrappedData = await scraperService.setProducts();
 
             res.status(200).json({message: "Scraped data saved to database"})
         }catch{
-            console.error('Error occurred while scraping and saving data:', error);
+            console.error('Error occurred while scraping and saving data:');
             res.status(500).json({ error: 'Internal server error' });
         }
     }
