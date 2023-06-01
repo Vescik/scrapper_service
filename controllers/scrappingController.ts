1aa9d2c4725bc3906bfe774d31fa65b3b9451dbe
Vescik <wescik992@gmail.com>
2023-06-01 20:12:56 +0200

    ready to use controller invoking scraping script

--- controllers/scrappingController.ts
+++ controllers/scrappingController.ts
@@ -1,15 +1,18 @@
+// scrapingController.js
 const scraperService = require('../services/scraperService');
 
 const scrapingController = {
-    async saveScrapData(req,res){
-        try{
-            const scrappedData = await scraperService.setProducts();
+  async scrapeAndSaveData(req, res) {
+    try {
+      const scrappedData = await scraperService.setProducts();
+      // TODO: Save the scrappedData to MongoDB using the appropriate model
 
-            res.status(200).json({message: "Scraped data saved to database"})
-        }catch{
-            console.error('Error occurred while scraping and saving data:');
-            res.status(500).json({ error: 'Internal server error' });
-        }
+     // res.status(200).json({ message: 'Scraped data saved to database' });
+    } catch (error) {
+      console.error('Error occurred while scraping and saving data:', error);
+    //  res.status(500).json({ error: 'Internal server error' });
     }
+  }, 
+};
 
-}
+module.exports = scrapingController;
