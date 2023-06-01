6a6091f86a5394d92f54eb8388d50a6b340ba05d
Vescik <wescik992@gmail.com>
2023-06-01 20:14:02 +0200

    implemented schedule to scrap web once a day

--- scripts/scrapeScript.js
+++ scripts/scrapeScript.js
@@ -1,9 +1,13 @@
 const cron = require('node-cron');
 const scrapingController = require('../controllers/scrappingController');
+const scheduleTimer = '10 20 * * *';
 
-// Run the scraping and saving script once per day at 12:00 PM
-cron.schedule('0 12 * * *', () => {
-  console.log('Running scraping script...');
-  scrapingController.scrapeAndSaveData();
-});
+const scrapeScript = () => {
+  console.log('Schedule timer: ', scheduleTimer);
+  cron.schedule(scheduleTimer, () => {
+    console.log('Running scraping script...');
+    scrapingController.scrapeAndSaveData();
+  });
+};
 
+module.exports = scrapeScript;
