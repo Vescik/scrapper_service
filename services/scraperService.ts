1a8e61837b9049315ef5cc4f37525394d83f59fe
Vescik <wescik992@gmail.com>
2023-06-01 18:22:33 +0200

    scraping service

--- services/scraperService.ts
+++ services/scraperService.ts
@@ -1,6 +1,14 @@
 const puppeteer = require('puppeteer');
 const url = "https://mcdonalds.pl/nasze-menu/"
 
+interface Product {
+    name: string;
+    image: string;
+    price: number;
+    type: string;
+    size: string;
+}
+
 const openWebPage = async (url:string) => {
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
@@ -9,20 +17,34 @@ const openWebPage = async (url:string) => {
 }
 
 
-const scrapProducts = async ()=> {
+
+const scrapData = async ()=> {
     const page = await openWebPage(url);
     
-    const productTexts = await page.$$eval('.product_thumb-text', (elements) =>{
+    const products = await page.$$eval('.product_thumb-text', (elements) =>{
         return elements.map((el) => el.textContent.trim())
     })
     
-    const imageUrls = await page.evaluate(() => {
+    const productsImg = await page.evaluate(() => {
         const images = Array.from(document.querySelectorAll('div.product_thumb > a > img'));
         return images.map((img:any) => img.src);
       });
 
-      return{ productTexts, imageUrls}
+      return{ products, productsImg}
+    }
+
+const pushToMenu = async () => {
+    const menu:Product[] = [];
+    const {products, productsImg} = await scrapData();
+    products.forEach((productText:string, index) => {
+        const product:Product = {
+            name: productText,
+            image: productsImg[index],
+            price: 0,
+            type: "",
+            size: ""
+        }
+        menu.push(product)
+    })  
 }
-
-
-module.exports = scrapProducts
+module.exports = pushToMenu
